8a06e1b479b230bb5c2095c93a640502a4f570ef
pradip12patel <patel9826ravi@gmail.com>
2023-02-19 16:46:09 +0530

    changes in conts class

--- src/main/java/Resources/ConstantsLogin.java
+++ src/main/java/Resources/ConstantsLogin.java
@@ -6,7 +6,7 @@ public class ConstantsLogin {
 	 public static String password1="ravi123";
 	 public static String username2="ravi2";
 	 public static String password2="ravi12";
-	 public static String username3="ravi2";
+	 public static String username3="ravi3";
 	 public static String password3="ravi1234";
 	 public static String experrormsg="Please check your username and password. If you still can't log in, contact your Salesforce administrator.";
 }
